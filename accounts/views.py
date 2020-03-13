63e699fd88f96ce070f99f83eb399df0a0c358f9
skybear308 <skybearok@outlook.com>
2020-03-13 08:02:19 -0500

    fourth-1 upload

---
+++ accounts/views.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import login
+from .forms import SignUpForm
+from django.shortcuts import render, redirect
+
+# Create your views here.
+def signup(request):
+    if request.method == 'POST':        
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        
+    return render(request, 'signup.html', {'form': form})
